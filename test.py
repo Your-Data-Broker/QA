798146ef2811de19bd176e95e6d52b2ac68e4b24
DmytroVi <somethingeasywhynot@gmail.com>
2026-05-05 19:26:26 +0300

    file update

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import cv2
 
-r, g, b = int(input("r: ")), int(input("g: ")), int(input("b: "))
-
 img = cv2.imread("testImg.jpg") #3D
 
-print(len(img))
-print(len(img[0]))
+res_y = len(img)
+res_x = len(img[0])
+
+class shape():
+    def __init__(self):
+        self.x_max = res_x
+        self.x_min = 0
+        self.y_max = res_y
+        self.y_min = 0
+        self.rgb = [256, 256, 256]
+
+r, g, b = int(input("r: ")), int(input("g: ")), int(input("b: "))
+
+
